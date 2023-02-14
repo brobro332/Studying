36194ad0280b1f8714b4ab9eaf61d0afb653af5a
brobro332 <93824318+brobro332@users.noreply.github.com>
2023-02-14 11:57:19 +0900

    20th commit

---
+++ JSPPrj/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -0,0 +1,31 @@
+package com.newlecture.web.service;
+
+import java.util.List;
+
+import com.newlecture.web.entity.Notice;
+
+public class NoticeService {
+	public List<Notice> getNoticeList(){
+		return null;
+	}
+	
+	public List<Notice> getNoticeList(int page){
+		return null;
+	}
+	
+	public List<Notice> getNoticeList(Sting field, String query, int page){
+		return null;
+	}
+	
+	public int getNoticeCount(){
+		return 0;
+	}
+	
+	public int getNoticeCount(String field, String query){
+		return 0;
+	}
+	
+	public List<Notice> getNoticeList(){
+		return null;
+	}
+}
