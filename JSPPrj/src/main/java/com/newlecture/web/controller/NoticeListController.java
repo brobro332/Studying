665f3ff5c55b1b1fe02cd9412a50a52295f981be
brobro332 <93824318+brobro332@users.noreply.github.com>
2023-02-14 17:37:27 +0900

    21th commit

--- JSPPrj/src/main/java/com/newlecture/web/controller/NoticeListController.java
+++ JSPPrj/src/main/java/com/newlecture/web/controller/NoticeListController.java
@@ -17,59 +17,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @WebServlet("/notice/list")
 public class NoticeListController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		List<Notice> list = new ArrayList<>();
-		
-		
-		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
-		String sql = "SELECT * FROM NOTICE";
-
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.getConnection(url,"newlec", "1234");
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-
-			while(rs.next()){
-
-				int id = rs.getInt("ID");
-				String title = rs.getString("TITLE");
-				Date regDate = rs.getDate("REGDATE");
-				String writerId = rs.getString("WRITER_ID");
-				String hit = rs.getString("HIT");
-				String files = rs.getString("FILES");
-				String content = rs.getString("CONTENT");
-
-			Notice notice = new Notice(
-								id,
-								title,
-								regDate,
-								writerId,
-								hit,
-								files,
-								content
-							);
-				list.add(notice);
-			}
-			rs.close();
-			st.close();
-			con.close();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		NoticeService service = new NoticeService();
+		List<Notice> list = service.getNoticeList();
+	
 		//forward 
 		request.setAttribute("list", list);
-		
 		request.getRequestDispatcher("/WEB-INF/view/notice/list.jsp").forward(request, response);
 	}
 }
