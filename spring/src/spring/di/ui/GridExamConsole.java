f6dbb5d2eb2568ae32d562c1f430c62193fe5aca
brobro332 <93824318+brobro332@users.noreply.github.com>
2023-02-22 03:53:43 +0900

    4th commit

--- spring/src/spring/di/ui/GridExamConsole.java
+++ spring/src/spring/di/ui/GridExamConsole.java
@@ -1,5 +1,8 @@
 package spring.di.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import spring.di.entity.Exam;
 
 public class GridExamConsole implements ExamConsole {
@@ -19,7 +22,7 @@ public class GridExamConsole implements ExamConsole {
 		System.out.println("┌─────────┬─────────┐");
 		System.out.println("│  total  │   avg   │");
 		System.out.println("├─────────┼─────────┤");
-		System.out.printf("│   %3d   │  %3.2f  │\n",exam.total(),exam.avg());
+		System.out.printf("│   %3d   │  %3.2f   │\n",exam.total(),exam.avg());
 		System.out.println("└─────────┴─────────┘");
 	}
 
--- spring/src/spring/di/ui/InlineExamConsole.java
+++ spring/src/spring/di/ui/InlineExamConsole.java
@@ -1,13 +1,20 @@
 package spring.di.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import spring.di.entity.Exam;
 
+@Component("console")
 public class InlineExamConsole implements ExamConsole {
 
+	@Autowired
+	//@Qualifier("exam2")
 	private Exam exam;
 	
 	public InlineExamConsole() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	public InlineExamConsole(Exam exam) {
@@ -16,8 +23,10 @@ public class InlineExamConsole implements ExamConsole {
 
 	@Override
 	public void print() {
-		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
-
+		if(exam == null)
+			System.out.printf("total is %d, avg is %f\n", 0, 0.0);
+		else
+			System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
 	}
 
 	@Override
