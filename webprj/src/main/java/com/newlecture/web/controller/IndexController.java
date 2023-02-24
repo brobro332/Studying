3ef3cbb7c29d5ae0a4f53dd9cb11da12712409c4
brobro332 <93824318+brobro332@users.noreply.github.com>
2023-02-24 18:06:31 +0900

    4th commit

--- webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -8,10 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class IndexController implements Controller {
 
+
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		ModelAndView mv = new ModelAndView("index");
+		ModelAndView mv = new ModelAndView("root.index");
 		mv.addObject("data", "Hello Spring MVC~");
 		// mv.setViewName("/WEB-INF/view/index.jsp");
 		
