0c15cc2015da0576d279614781b5f67f6907a86f
brobro332 <93824318+brobro332@users.noreply.github.com>
2023-03-05 23:59:17 +0900

    2nd commit

--- study/board/src/main/java/com/study/board/controller/BoardController.java
+++ study/board/src/main/java/com/study/board/controller/BoardController.java
@@ -1,15 +1,26 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
-    @GetMapping("/")
-    @ResponseBody
-    public String main() {
+    @Autowired
+    private BoardService boardService;
+    @GetMapping("/board/write") //localhost:8080/board/write
+    public String boardWriteForm() {
 
-        return "Hello world";
+        return "boardwrite";
+    }
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+
+        return "";
     }
 }
