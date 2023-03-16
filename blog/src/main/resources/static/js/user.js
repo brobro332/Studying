8118477797f2dc547f7d798f22e67d4abfff0a7e
brobro332 <93824318+brobro332@users.noreply.github.com>
2023-03-16 17:07:52 +0900

    12th commit

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
 			$("#btn-save").on("click", ()=>{ // 화살표 함수를 쓰는 이유 : this를 바인딩 하기 위함
 				this.save();
 			});
+			$("#btn-login").on("click", ()=>{ // 화살표 함수를 쓰는 이유 : this를 바인딩 하기 위함
+				this.login();
+			});
 		},
 
 		save: function() {
@@ -28,6 +31,27 @@ let index = {
 			}).done(function(resp) {
 				alert("회원가입이 완료되었습니다.");
 				// alert(resp)
+				location.href = "/blog";
+			}).fail(function(error) {
+				alert(JSON.stringify(error));
+			}); 
+		},
+		
+		login: function() {
+			// alert("user의 login 함수 호출됨.");
+			let data = {
+					username: $("#username").val(),
+					password: $("#password").val()
+			}
+			
+			$.ajax({
+				type: "POST",
+				url: "/blog/api/user/login",
+				data: JSON.stringify(data), // HTTP body 데이터
+				contentType: "application/json; charset=utf-8", // body 데이터가 어떤 타입인지
+				dataType: "json" // 요청을 통해 서버에서 응답이 왔을 때 json이라면 -> javascript 오브젝트로 변경
+			}).done(function(resp) {
+				alert("로그인이 완료되었습니다.");			
 				location.href = "/blog";
 			}).fail(function(error) {
 				alert(JSON.stringify(error));
