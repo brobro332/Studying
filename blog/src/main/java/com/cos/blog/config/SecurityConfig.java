7d753a78b5fa99440fe63b01eb2c29762caec69a
brobro332 <93824318+brobro332@users.noreply.github.com>
2023-03-18 01:46:24 +0900

    15th commit

--- blog/src/main/java/com/cos/blog/config/SecurityConfig.java
+++ blog/src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,20 +1,35 @@
 package com.cos.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.cos.blog.config.auth.PrincipalDetailService;
 
 //Bean 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
 @Configuration // IoC 관리
 public class SecurityConfig {
 	
+	@Autowired
+	private PrincipalDetailService principalDetailService; 
+	
 	@Bean
-	 BCryptPasswordEncoder encode() {
+	 BCryptPasswordEncoder encodePWD() {
 	  return new BCryptPasswordEncoder();
 	 }
 
+	// 시큐리티가 대신 로그인하며 password를 가로채는데
+	// 해당 password가 뭘로 해쉬가 되어 회원가입이 되었는지 알아야 
+	// 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
+
+	private void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
+	}
+	
 	 @Bean
 	 SecurityFilterChain configure(HttpSecurity http) throws Exception {
 	  http
@@ -26,7 +41,9 @@ public class SecurityConfig {
 	    	.authenticated() // 인증이 돼야 한다.
 	    .and() // 그리고
 	    	.formLogin() // 인증이 돼야 하면
-	    	.loginPage("/auth/loginForm"); // 로그인 페이지로 보내라.
+	    	.loginPage("/auth/loginForm") // 로그인 페이지로 보내라.
+	    	.loginProcessingUrl("/auth/loginProc") // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인해준다. 
+	  		.defaultSuccessUrl("/"); // 정상적으로 로그인되면 "/"로 이동
 	  
 	  return http.build();
 	 }
--- blog/src/main/java/com/cos/blog/repository/UserRepository.java
+++ blog/src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,7 +12,8 @@ import com.cos.blog.model.User;
 // 자동으로 bean 등록이 된다.
 // @Repository // 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	// JPA Naming 쿼리 : SELECT * FROM user WHERE username = ?1;
+	Optional<User> findByUsername(String username); 
 }
 
 // JPA Naming 쿼리
