1e2aa0b53c59a05a0de0a350199d0918f1d5b489
332bro <93824318+brobro332@users.noreply.github.com>
2023-03-24 20:42:53 +0900

    13th commit

--- blog/src/main/java/com/cos/blog/service/BoardService.java
+++ blog/src/main/java/com/cos/blog/service/BoardService.java
@@ -3,15 +3,14 @@ package com.cos.blog.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.model.Board;
-import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
-import com.cos.blog.repository.UserRepository;
 
 @Service
 public class BoardService {
@@ -25,4 +24,10 @@ public class BoardService {
 		board.setUserId(user);
 		boardRepository.save(board);
 	}
+	
+	public Page<Board> 글목록(Pageable pageable) {
+		
+		return boardRepository.findAll(pageable);
+	}
+	
 }
